20f3f4cf4d3802baa59223b9990e6c8a518152cc
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2023-11-06 19:57:27 +0100

    Fixed UI Issues and changed the name of a project

--- portfolio-page/json/projects/index.js
+++ portfolio-page/json/projects/index.js
@@ -1,7 +1,7 @@
 import R6CreativeHub from "./r6-creative-hub.json";
 import PortfolioPage from "./portfolio-page.json";
 import CollatzConjecture from "./collatz-conjecture.json";
-import Fivem from "./Fivem-Automatic-Start-And-Vehicle-List.json";
+import Fivem from "./FiveM-Utils.json";
 
 const projects = [Fivem, CollatzConjecture, R6CreativeHub, PortfolioPage];
 
