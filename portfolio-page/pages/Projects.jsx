e756a2a819f5e9d4c935549828aa484c968a86d1
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2023-11-11 19:20:36 +0100

    Added Filters to the Projects page

--- portfolio-page/pages/Projects.jsx
+++ portfolio-page/pages/Projects.jsx
@@ -1,25 +1,136 @@
 import React from "react";
 import TechnologieBadge from "../components/TechnologieBadge";
 import projects from "../json/projects/";
+import { Filter, Hammer, ShieldCheck } from "lucide-react";
+import { useState, useEffect } from "react";
 
 
 const Projects = () => {
+    const [openFilter, setOpenFilter] = useState(false);
+    const [filter, setFilter] = useState({
+        status: [],
+        size: [],
+        lang: []
+    });
+    const [filteredProjects, setFilteredProjects] = useState([]);
+
+    useEffect(() => {
+        setFilteredProjects(projects);
+    }, []);
+
+    function handleFilterApply() {
+        var newProjects = [];
+        projects.forEach((project) => {
+            var includesStatus = filter.status.some(f => project.tags.includes(f));
+            if (filter.status.length == 0) includesStatus = true;
+            var includesSize = filter.size.some(f => project.tags.includes(f));
+            if (filter.size.length == 0) includesSize = true;
+            var includesLang = filter.lang.some(f => project.tags.includes(f));
+            if (filter.lang.length == 0) includesLang = true;
+
+            if (includesStatus && includesSize && includesLang) newProjects.push(project);
+        });
+        setFilteredProjects(newProjects);
+        setOpenFilter(false);
+    }
+
     return (
         <div className="relative min-h-[90vh] z-0 flex justify-center">
             <div className="relative top-16 max-w-[90vw] w-[90%]">
                 <h1 className="text-4xl 2xl:text-5xl leading-20 font-bold tracking-widest uppercase mb-4">Projects</h1>
                 <div className="flex items-center flex-col pb-32">
                     <div className="h-2 w-10 bg-gradient3 mb-4 rounded-xl"></div>
-                    {projects.map((project) =>
-                        <div className="bg-[#242424] mb-4 bg-opacity-60 border-[#242424] border-2 w-full xl:w-1/2 rounded-md p-2">
+                    <div className="flex justify-left w-full xl:w-1/2">
+                        <button onClick={() => setOpenFilter(!openFilter)} className="bg-[#242424] mb-4 bg-opacity-60 border-[#242424] border-2 rounded-md p-2 font-bold"><Filter className="inline mr-2" />Filter by Tags</button>
+                        <div className={`${openFilter ? "block" : "hidden"} absolute bg-[#242424] mt-12 shadow-lg border-[#242424] border-2 rounded-md p-2 font-bold`}>
+                            <h4 className="text-white/50 text-left mt-2 border-b border-white/50">Project Status</h4>
+                            <div className="flex flex-row-reverse items-center justify-end mb-1">
+                                <label htmlFor="finished"><ShieldCheck className="inline mr-1" />Finished</label>
+                                <input
+                                    onChange={(e) => e.target.checked ? setFilter({ ...filter, status: [...filter.status, "finished"] }) : setFilter({ ...filter, status: filter.status.filter((f) => { return f != "finished" }) })}
+                                    className="mr-2 h-4 w-4"
+                                    type="checkbox"
+                                    name="finished"
+                                    id="finished" />
+                            </div>
+                            <div className="flex flex-row-reverse items-center justify-end mb-1">
+                                <label htmlFor="wip"><Hammer className="inline mr-1" />Work in Progress</label>
+                                <input
+                                    onChange={(e) => e.target.checked ? setFilter({ ...filter, status: [...filter.status, "wip"] }) : setFilter({ ...filter, status: filter.status.filter((f) => { return f != "wip" }) })}
+                                    className="mr-2 h-4 w-4"
+                                    type="checkbox"
+                                    name="wip"
+                                    id="wip" />
+                            </div>
+                            <h4 className="text-white/50 text-left mt-2 border-b border-white/50">Project Size</h4>
+                            <div className="flex flex-row-reverse items-center justify-end mb-1">
+                                <label htmlFor="small">Small</label>
+                                <input
+                                    onChange={(e) => e.target.checked ? setFilter({ ...filter, size: [...filter.size, "small"] }) : setFilter({ ...filter, size: filter.size.filter((f) => { return f != "small" }) })}
+                                    className="mr-2 h-4 w-4"
+                                    type="checkbox"
+                                    name="small"
+                                    id="small" />
+                            </div>
+                            <div className="flex flex-row-reverse items-center justify-end mb-1">
+                                <label htmlFor="medium">Medium</label>
+                                <input
+                                    onChange={(e) => e.target.checked ? setFilter({ ...filter, size: [...filter.size, "medium"] }) : setFilter({ ...filter, size: filter.size.filter((f) => { return f != "medium" }) })}
+                                    className="mr-2 h-4 w-4"
+                                    type="checkbox"
+                                    name="medium"
+                                    id="medium" />
+                            </div>
+                            <div className="flex flex-row-reverse items-center justify-end mb-1">
+                                <label htmlFor="large">Large</label>
+                                <input
+                                    onChange={(e) => e.target.checked ? setFilter({ ...filter, size: [...filter.size, "large"] }) : setFilter({ ...filter, size: filter.size.filter((f) => { return f != "large" }) })}
+                                    className="mr-2 h-4 w-4"
+                                    type="checkbox"
+                                    name="large"
+                                    id="large" />
+                            </div>
+                            <h4 className="text-white/50 text-left mt-2 border-b border-white/50">Language</h4>
+                            <div className="flex flex-row-reverse items-center justify-end mb-1">
+                                <label className="flex items-center" htmlFor="js">
+                                    <img src="/technologie-icons/icons8-js.svg" alt="JavaScript" className="inline h-5 mr-1" />
+                                    JavaScript
+                                </label>
+                                <input
+                                    onChange={(e) => e.target.checked ? setFilter({ ...filter, lang: [...filter.lang, "js"] }) : setFilter({ ...filter, lang: filter.lang.filter((f) => { return f != "js" }) })}
+                                    className="mr-2 h-4 w-4"
+                                    type="checkbox"
+                                    name="js"
+                                    id="js" />
+                            </div>
+                            <div className="flex flex-row-reverse items-center justify-end mb-1">
+                                <label className="flex items-center" htmlFor="python">
+                                    <img src="/technologie-icons/icons8-python.svg" alt="Python" className="inline h-5 mr-1" />
+                                    Python
+                                </label>
+                                <input
+                                    onChange={(e) => e.target.checked ? setFilter({ ...filter, lang: [...filter.lang, "python"] }) : setFilter({ ...filter, lang: filter.lang.filter((f) => { return f != "python" }) })}
+                                    className="mr-2 h-4 w-4"
+                                    type="checkbox"
+                                    name="python"
+                                    id="python" />
+                            </div>
+                            <div className="flex justify-left mt-4">
+                                <button onClick={handleFilterApply} className="tracking-tight w-full sm:w-fit px-8 py-1 transition-all border-2 rounded-md border-[#fb5607] text-[#fb5607] hover:shadow-[0_0_35px_-3px_#fb5607] hover:bg-[#fb5607] hover:text-white">Apply</button>
+                            </div>
+                        </div>
+                    </div>
+                    {filteredProjects.map((project, i) =>
+                        <div key={i} className="bg-[#242424] mb-4 bg-opacity-60 border-[#242424] border-2 w-full xl:w-1/2 rounded-md p-2">
                             <a href={"/project/" + project.filename}>
-                                <h2 className="text-left px-2 pt-2 font-bold text-2xl line-clamp-1">{project.title}</h2>
+                                <h2 className="text-left px-2 pt-2 font-bold text-2xl line-clamp-1">{project.title}{project.tags.includes("wip") && (<span className={"inline-flex items-center cursor-default text-sm font-bold px-2 ml-2 mb-2 rounded-xl border-2"}><Hammer className="inline h-4" />W.I.P.</span>)}</h2>
                                 <div className="flex flex-wrap px-2 my-2 xl:my-0">
                                     <TechnologieBadge technologies={project.technologies} type={"badge"} />
                                 </div>
                                 <p dangerouslySetInnerHTML={{ __html: project.overview }} className="text-left text-white/50 line-clamp-2 px-2 max-w-2xl">
 
-                                </p></a>
+                                </p>
+                            </a>
                         </div>
                     )}
                 </div>
