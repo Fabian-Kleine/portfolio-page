b8b7b7a7c46995e10a886ef503ca888bc867d804
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2023-12-02 16:32:20 +0100

    Added Product Filter Project

--- portfolio-page/json/projects/index.js
+++ portfolio-page/json/projects/index.js
@@ -2,9 +2,10 @@ import R6CreativeHub from "./r6-creative-hub.json";
 import PortfolioPage from "./portfolio-page.json";
 import CollatzConjecture from "./collatz-conjecture.json";
 import Fivem from "./FiveM-Utils.json";
-import RheinWaalEvents from "./rhein-waal-events.json"
-import CollatzConjectureR from "./collatz-conjecture-r.json"   
+import RheinWaalEvents from "./rhein-waal-events.json";
+import CollatzConjectureR from "./collatz-conjecture-r.json";
+import Oleon from "./product-filter.json" 
 
-const projects = [CollatzConjectureR, RheinWaalEvents, Fivem, CollatzConjecture, R6CreativeHub, PortfolioPage];
+const projects = [Oleon, CollatzConjectureR, RheinWaalEvents, Fivem, CollatzConjecture, R6CreativeHub, PortfolioPage];
 
 export default projects;
