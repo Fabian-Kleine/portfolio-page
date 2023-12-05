d464f95d173b0b64088884f978ca11ea3069baa2
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2023-12-05 16:04:07 +0100

    Added Steam UI Project

--- portfolio-page/json/projects/index.js
+++ portfolio-page/json/projects/index.js
@@ -4,8 +4,9 @@ import CollatzConjecture from "./collatz-conjecture.json";
 import Fivem from "./FiveM-Utils.json";
 import RheinWaalEvents from "./rhein-waal-events.json";
 import CollatzConjectureR from "./collatz-conjecture-r.json";
-import Oleon from "./product-filter.json" 
+import Oleon from "./product-filter.json";
+import Steam from "./steam-inspired-ui.json"
 
-const projects = [Oleon, CollatzConjectureR, RheinWaalEvents, Fivem, CollatzConjecture, R6CreativeHub, PortfolioPage];
+const projects = [Steam, Oleon, CollatzConjectureR, RheinWaalEvents, Fivem, CollatzConjecture, R6CreativeHub, PortfolioPage];
 
 export default projects;
