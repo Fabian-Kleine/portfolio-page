ff17c47cef16796cfa0167c4e1df0f78188b81e4
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-01-02 20:50:26 +0100

    added cdn-server project

--- portfolio-page/json/projects/index.js
+++ portfolio-page/json/projects/index.js
@@ -5,8 +5,9 @@ import Fivem from "./FiveM-Utils.json";
 import RheinWaalEvents from "./rhein-waal-events.json";
 import CollatzConjectureR from "./collatz-conjecture-r.json";
 import Oleon from "./product-filter.json";
-import Steam from "./steam-inspired-ui.json"
+import Steam from "./steam-inspired-ui.json";
+import CDN from "./cdn-server.json"
 
-const projects = [Steam, Oleon, CollatzConjectureR, RheinWaalEvents, Fivem, CollatzConjecture, R6CreativeHub, PortfolioPage];
+const projects = [CDN, Steam, Oleon, CollatzConjectureR, RheinWaalEvents, Fivem, CollatzConjecture, R6CreativeHub, PortfolioPage];
 
 export default projects;
