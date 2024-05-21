c96830ef446012c0b542690be53b6af0f1854333
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-05-21 13:04:05 +0200

    feat: add gap between icon and text

--- components/TechnologieBadge.jsx
+++ components/TechnologieBadge.jsx
@@ -23,7 +23,7 @@ const TechnologieBadge = ({ technologies, type }) => {
                 {renderedTechnologie.map((tech, i) =>
                     <span style={{
                         '--dynamic-color': `${hexToRgb(tech.color).r}, ${hexToRgb(tech.color).g}, ${hexToRgb(tech.color).b}`,
-                    }} key={i} className={"flex items-center cursor-default text-sm font-bold px-2 mr-2 mb-2 rounded-xl border-techBadge border-2 bg-techBadge bg-opacity-60"}>
+                    }} key={i} className={"flex justify-center items-center cursor-default text-sm font-bold px-2 mr-2 mb-2 rounded-xl border-techBadge border-2 bg-techBadge bg-opacity-60"}>
                         <img src={tech.icon} alt={tech.name} className="h-5 mr-1" />
                         {tech.name}
                     </span>
@@ -37,9 +37,9 @@ const TechnologieBadge = ({ technologies, type }) => {
                     <div key={i} style={{
                         '--dynamic-color': `${hexToRgb(tech.color).r}, ${hexToRgb(tech.color).g}, ${hexToRgb(tech.color).b}`,
                         '--dynamic-shadow-color': `${hexToRgb(tech.hover).r}, ${hexToRgb(tech.hover).g}, ${hexToRgb(tech.hover).b}`,
-                    }} className={"transition-shadow cursor-default mr-6 mb-6 py-2 px-4 bg-techBadge border-2 rounded-lg border-techBadge bg-opacity-60 hover:shadow-techBadge"}>
-                        <img src={tech.icon} alt={tech.name} className="inline h-10" />
-                        <h4 className="inline font-bold">{tech.name}</h4>
+                    }} className={"flex justify-center items-center gap-1 transition-shadow cursor-default mr-6 mb-6 py-2 px-4 bg-techBadge border-2 rounded-lg border-techBadge bg-opacity-60 hover:shadow-techBadge"}>
+                        <img src={tech.icon} alt={tech.name} className="h-10" />
+                        <h4 className="font-bold">{tech.name}</h4>
                     </div>
                 )}
             </>
